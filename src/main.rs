ee7862c582cdfc6ed9e6bea82fcecde906077cb7
eynol <625608852@qq.com>
2023-11-28 05:11:25 +0800

    fix: 调整结构

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
 use console::{style, Attribute};
+use yodaodict::DictResult;
 mod yodaodict;
-use std::{env, path::PathBuf};
-
-use xml::{
-    reader::{EventReader, XmlEvent},
-    ParserConfig,
-};
+use std::env;
 
 struct CliOptions {
     debug: bool,
 }
 
 static mut DEBUG_MODE: bool = false;
-#[derive(Debug)]
-struct DictResult {
-    query: String,
-    return_phrase: Option<String>,
-    lang: String,
-    phonetic_symbol: Option<String>,
-    translation: Option<CustomTranslation>,
-    web_translation: Option<Vec<WebTranslation>>,
-    wiki: Option<Wiki>,
-}
-#[derive(Debug)]
-struct CustomTranslation {
-    kind: String,
-    content: Vec<String>,
-}
-#[derive(Debug)]
-struct WebTranslation {
-    key: String,
-    value: Vec<String>,
-}
 
-#[derive(Debug)]
-struct Wiki {
-    entry: String,
-    summary: String,
-}
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let tt = vec![1, 2, 3, 4, 5];
     // test(tt);
@@ -82,7 +53,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if options.debug {
         println!(" xml data:{} \n", &resp)
     }
-    let dict_rs = get_xml_node_list(&word, &resp);
+    let dict_rs = yodaodict::get_xml_node_list(&word, &resp);
     if options.debug {
         println!("xml is {} \n", resp);
         println!("dict rs is {:#?}", dict_rs);
@@ -95,113 +66,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     print_wiki(&dict_rs);
     // println!("{:#?}", resp);
     Ok(())
-}
-
-struct XmlData {
-    cdata: String,
-    character: String,
-}
-
-fn get_xml_node_list<'a>(word: &String, xmldata: &'a str) -> DictResult {
-    let mut dict_rs = DictResult {
-        query: word.clone(),
-        return_phrase: None,
-        lang: String::from("eng"),
-        phonetic_symbol: None,
-        translation: None,
-        web_translation: None,
-        wiki: None,
-    };
-
-    let empty_str = "".to_string();
-    let config = ParserConfig {
-        trim_whitespace: true,
-        ..ParserConfig::default()
-    };
-    let parser = EventReader::new_with_config(xmldata.as_bytes(), config);
-
-    let mut temp_data = XmlData {
-        cdata: empty_str.clone(),
-        character: empty_str.clone(),
-    };
-    let mut path_buf = PathBuf::new();
-
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                if name.local_name != "yodaodict" {
-                    path_buf.push(name.local_name);
-                }
-            }
-            Ok(XmlEvent::CData(data)) => {
-                temp_data.cdata = data;
-            }
-            Ok(XmlEvent::Characters(data)) => {
-                temp_data.character = data;
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                if path_buf.ends_with("return-phrase") {
-                    dict_rs.return_phrase = Some(temp_data.cdata + &temp_data.character);
-                } else if path_buf.ends_with("custom-translation/type") {
-                    let dict = dict_rs.translation.get_or_insert(CustomTranslation {
-                        kind: empty_str.clone(),
-                        content: vec![],
-                    });
-                    dict.kind = temp_data.character;
-                } else if path_buf.ends_with("custom-translation/translation/content") {
-                    if let Some(trans) = &mut dict_rs.translation {
-                        trans.content.push(temp_data.cdata);
-                    };
-                } else if path_buf.ends_with("phonetic-symbol") {
-                    dict_rs.phonetic_symbol = Some(temp_data.character)
-                } else if path_buf.ends_with("web-translation/key") {
-                    let list = dict_rs.web_translation.get_or_insert(Vec::new());
-                    list.push(WebTranslation {
-                        key: temp_data.cdata + &temp_data.character,
-                        value: vec![],
-                    });
-                } else if path_buf.ends_with("web-translation/trans/value") {
-                    if let Some(list) = dict_rs.web_translation.as_mut().unwrap().last_mut() {
-                        list.value.push(temp_data.cdata + &temp_data.character);
-                    };
-                } else if path_buf.ends_with("web-translation/trans/value/cl") {
-                    if temp_data.cdata.len() > 0 || temp_data.character.len() > 0 {
-                        // 如果是计算机目录类别的，需要合并进前一个的value里面
-                        if let Some(web_translation) =
-                            dict_rs.web_translation.as_mut().unwrap().last_mut()
-                        {
-                            let last_value = web_translation.value.last_mut().unwrap();
-                            last_value.push_str("(");
-                            last_value.push_str(&temp_data.cdata);
-                            last_value.push_str(&temp_data.character);
-                            last_value.push_str(")");
-                        };
-                    }
-                } else if path_buf.ends_with("wiki-entry/info/data/entry") {
-                    dict_rs.wiki = Some(Wiki {
-                        entry: temp_data.character,
-                        summary: empty_str.clone(),
-                    });
-                } else if path_buf.ends_with("wiki-entry/info/data/summary") {
-                    dict_rs.wiki.as_mut().unwrap().summary = temp_data.character;
-                }
-
-                // println!("{}", path_buf.display());
-                temp_data.cdata = empty_str.clone();
-                temp_data.character = empty_str.clone();
-                if !name.to_string().eq("yodaodict") {
-                    path_buf.pop();
-                }
-                // println!("{}-{}", indent(depth), name);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
-            }
-            _ => {}
-        }
-    }
-    dict_rs
 }
 
 // 打印基础信息
--- src/yodaodict.rs
+++ src/yodaodict.rs
@@ -1,5 +1,10 @@
+use std::path::PathBuf;
+
 use url::Url;
-use xml::reader::{EventReader, XmlEvent};
+use xml::{
+    reader::{EventReader, XmlEvent},
+    ParserConfig,
+};
 const  TRANSLATE_URL:&str = "https://dict.youdao.com/fsearch?version=1.1&client=deskdict&keyfrom=chrome.extension&doctype=xml&xmlVersion=3.2";
 
 // Language: rust
@@ -9,56 +14,128 @@ pub fn build_query_url(word: &str) -> String {
     parsed.query_pairs_mut().append_pair("q", word);
     parsed.to_string()
 }
-struct XmlData {
+pub struct XmlData {
     cdata: String,
     character: String,
 }
+
+#[derive(Debug)]
+pub struct DictResult {
+    pub query: String,
+    pub return_phrase: Option<String>,
+    pub lang: String,
+    pub phonetic_symbol: Option<String>,
+    pub translation: Option<CustomTranslation>,
+    pub web_translation: Option<Vec<WebTranslation>>,
+    pub wiki: Option<Wiki>,
+}
+#[derive(Debug)]
+pub struct CustomTranslation {
+    pub kind: String,
+    pub content: Vec<String>,
+}
+#[derive(Debug)]
+pub struct WebTranslation {
+    pub key: String,
+    pub value: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct Wiki {
+    pub entry: String,
+    pub summary: String,
+}
+
 static EMPTY_STR: &str = "";
-pub fn get_xml_node_list<'a>(xmldata: &'a str) -> Vec<(Vec<String>, String)> {
-    let mut xml_node_list: Vec<(Vec<String>, String)> = Vec::new();
-    let parser = EventReader::from_str(xmldata);
-    let mut xml_paths: Vec<String> = Vec::new();
+pub fn get_xml_node_list<'a>(word: &String, xmldata: &'a str) -> DictResult {
+    let mut dict_rs = DictResult {
+        query: word.clone(),
+        return_phrase: None,
+        lang: String::from("eng"),
+        phonetic_symbol: None,
+        translation: None,
+        web_translation: None,
+        wiki: None,
+    };
+
+    let empty_str = "".to_string();
+    let config = ParserConfig {
+        trim_whitespace: true,
+        ..ParserConfig::default()
+    };
+    let parser = EventReader::new_with_config(xmldata.as_bytes(), config);
 
     let mut temp_data = XmlData {
-        cdata: EMPTY_STR.to_string(),
-        character: EMPTY_STR.to_string(),
+        cdata: empty_str.clone(),
+        character: empty_str.clone(),
     };
+    let mut path_buf = PathBuf::new();
 
     for e in parser {
         match e {
             Ok(XmlEvent::StartElement { name, .. }) => {
-                if !name.local_name.eq("yodaodict") {
-                    xml_paths.push(name.local_name);
+                if name.local_name != "yodaodict" {
+                    path_buf.push(name.local_name);
                 }
             }
             Ok(XmlEvent::CData(data)) => {
                 temp_data.cdata = data;
             }
             Ok(XmlEvent::Characters(data)) => {
-                temp_data.character = data.trim().to_string();
+                temp_data.character = data;
             }
             Ok(XmlEvent::EndElement { name }) => {
-                if temp_data.cdata.len() > 0 || temp_data.character.len() > 0 {
-                    // 如果是计算机目录类别的，需要合并进前一个的里面
-                    if let Some(key) = xml_paths.last() {
-                        if key.eq("cl") {
-                            let last_value = &mut xml_node_list.last_mut().unwrap().1;
+                if path_buf.ends_with("return-phrase") {
+                    dict_rs.return_phrase = Some(temp_data.cdata + &temp_data.character);
+                } else if path_buf.ends_with("custom-translation/type") {
+                    let dict = dict_rs.translation.get_or_insert(CustomTranslation {
+                        kind: empty_str.clone(),
+                        content: vec![],
+                    });
+                    dict.kind = temp_data.character;
+                } else if path_buf.ends_with("custom-translation/translation/content") {
+                    if let Some(trans) = &mut dict_rs.translation {
+                        trans.content.push(temp_data.cdata);
+                    };
+                } else if path_buf.ends_with("phonetic-symbol") {
+                    dict_rs.phonetic_symbol = Some(temp_data.character)
+                } else if path_buf.ends_with("web-translation/key") {
+                    let list = dict_rs.web_translation.get_or_insert(Vec::new());
+                    list.push(WebTranslation {
+                        key: temp_data.cdata + &temp_data.character,
+                        value: vec![],
+                    });
+                } else if path_buf.ends_with("web-translation/trans/value") {
+                    if let Some(list) = dict_rs.web_translation.as_mut().unwrap().last_mut() {
+                        list.value.push(temp_data.cdata + &temp_data.character);
+                    };
+                } else if path_buf.ends_with("web-translation/trans/value/cl") {
+                    if temp_data.cdata.len() > 0 || temp_data.character.len() > 0 {
+                        // 如果是计算机目录类别的，需要合并进前一个的value里面
+                        if let Some(web_translation) =
+                            dict_rs.web_translation.as_mut().unwrap().last_mut()
+                        {
+                            let last_value = web_translation.value.last_mut().unwrap();
                             last_value.push_str("(");
                             last_value.push_str(&temp_data.cdata);
                             last_value.push_str(&temp_data.character);
                             last_value.push_str(")");
-                        } else {
-                            xml_node_list.push((
-                                xml_paths.clone(),
-                                format!("{}{}", temp_data.cdata, temp_data.character),
-                            ));
-                        }
+                        };
                     }
+                } else if path_buf.ends_with("wiki-entry/info/data/entry") {
+                    dict_rs.wiki = Some(Wiki {
+                        entry: temp_data.character,
+                        summary: empty_str.clone(),
+                    });
+                } else if path_buf.ends_with("wiki-entry/info/data/summary") {
+                    dict_rs.wiki.as_mut().unwrap().summary = temp_data.character;
                 }
-                temp_data.cdata = EMPTY_STR.to_string();
-                temp_data.character = EMPTY_STR.to_string();
+
+                // println!("{}", path_buf.display());
+                temp_data.cdata = empty_str.clone();
+                temp_data.character = empty_str.clone();
                 if !name.to_string().eq("yodaodict") {
-                    xml_paths.pop();
+                    path_buf.pop();
                 }
                 // println!("{}-{}", indent(depth), name);
             }
@@ -69,5 +146,5 @@ pub fn get_xml_node_list<'a>(xmldata: &'a str) -> Vec<(Vec<String>, String)> {
             _ => {}
         }
     }
-    xml_node_list
+    dict_rs
 }
