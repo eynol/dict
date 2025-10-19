7e856cea6feba8e3d237c87f73fd108ef9c1f248
eynol <625608852@qq.com>
2025-10-20 03:47:42 +0800

    feat: 使用 quick-xml 优化

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,26 @@
-use console::{style, Attribute};
-use yodaodict::DictResult;
+use clap::{command, Parser};
 mod yodaodict;
 use std::env;
 
-struct CliOptions {
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Name of the person to greet
+    #[arg(long, default_value_t = false)]
     debug: bool,
-}
 
-static mut DEBUG_MODE: bool = false;
+    pub word: Option<String>,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let tt = vec![1, 2, 3, 4, 5];
-    // test(tt);
+    let args = Args::parse();
 
-    let mut args: Vec<String> = env::args().skip(1).collect();
-    // args.remove(0);
-
-    let debug_index = args.binary_search(&String::from("--debug"));
-
-    let mut options = CliOptions { debug: false };
-    if let Ok(idx) = debug_index {
-        args.remove(idx);
-        options.debug = true;
-    }
-
-    if args.len() < 1 {
-        println!("Usage: {} <单词>", args[0]);
+    if args.word.is_none() {
+        println!("Usage: dict <单词>");
         return Ok(());
     }
 
-    let word = args.get(0).unwrap();
-
-    // let word: &String = &args[1];
-    if word == "-v" || word == "--version" {
-        //  只查看version
-        const VERSION: &str = env!("CARGO_PKG_VERSION");
-        println!("v{}", VERSION);
-        return Ok(());
-    }
-
-    let url = yodaodict::build_query_url(word);
-    unsafe {
-        if options.debug {
-            DEBUG_MODE = options.debug;
-            let url = style(&url).attr(Attribute::Underlined);
-            println!("url is {url}",);
-        }
-    }
-
-    let resp = reqwest::blocking::get(url)?.text()?;
-
-    if options.debug {
-        println!(" xml data:{} \n", &resp)
-    }
-    let dict_rs = yodaodict::get_xml_node_list(&word, &resp);
-    if options.debug {
-        println!("xml is {} \n", resp);
-        println!("dict rs is {:#?}", dict_rs);
-    }
-
-    print_basic_info(&dict_rs);
-
-    print_tanslation_content(&dict_rs);
-    print_web_tanslation_content(&dict_rs);
-    print_wiki(&dict_rs);
-    // println!("{:#?}", resp);
+    yodaodict::search_word(args);
     Ok(())
-}
-
-// 打印基础信息
-fn print_basic_info(xml_node_list: &DictResult) {
-    if let Some(value) = &xml_node_list.return_phrase {
-        print!("  {}", style(value).bold().green());
-    }
-
-    if let Some(value) = &xml_node_list.phonetic_symbol {
-        print!("{}", style(format!("  [{}]", value)).bold().green().dim());
-    }
-    print!("\n")
-}
-
-// 打印翻译结果
-fn print_tanslation_content(xml_node_list: &DictResult) {
-    if let Some(trans) = &xml_node_list.translation {
-        print_section_title("翻译");
-        for value in trans.content.iter() {
-            println!("  {}", value);
-        }
-        print!("\n")
-    }
-}
-
-// 打印网络翻译结果
-fn print_web_tanslation_content(xml_node_list: &DictResult) {
-    if let Some(trans) = &xml_node_list.web_translation {
-        print_section_title("网络翻译");
-        let mut print_next_line = false;
-        for item in trans.iter() {
-            if !print_next_line {
-                print_next_line = true;
-            } else {
-                print!("\n");
-            }
-            print!("{} \n  ", style(&item.key).magenta());
-            // print!("({})", value);
-            item.value.iter().for_each(|v| {
-                print!("{}; ", v);
-            })
-        }
-        print!("\n")
-    }
-}
-
-fn print_section_title(title: &str) {
-    println!(
-        "========{txt:^width$}========",
-        txt = style(title).magenta().dim(),
-        width = 8
-    );
-}
-// 打印 Wiki 结果
-fn print_wiki(xml_node_list: &DictResult) {
-    if let Some(wiki) = &xml_node_list.wiki {
-        print_section_title("Wiki");
-        println!("{}", style(format!("[ {} ]", wiki.entry)).green());
-
-        println!("  {}", wiki.summary);
-        print!("\n")
-    }
 }
--- src/yodaodict.rs
+++ src/yodaodict.rs
@@ -1,150 +1,259 @@
-use std::path::PathBuf;
+use std::{borrow::Cow, fmt::Debug};
 
+use console::{style, Attribute};
+use quick_xml::de::from_str;
+use serde::Deserialize;
 use url::Url;
-use xml::{
-    reader::{EventReader, XmlEvent},
-    ParserConfig,
-};
+
+use crate::Args;
 const  TRANSLATE_URL:&str = "https://dict.youdao.com/fsearch?version=1.1&client=deskdict&keyfrom=chrome.extension&doctype=xml&xmlVersion=3.2";
 
-// Language: rust
-// 构建查询的url
-pub fn build_query_url(word: &str) -> String {
-    let mut parsed = Url::parse(TRANSLATE_URL).unwrap();
-    parsed.query_pairs_mut().append_pair("q", word);
-    parsed.to_string()
-}
-pub struct XmlData {
-    cdata: String,
-    character: String,
+struct YouDaoTranslator<'a> {
+    args: &'a Args,
 }
 
-#[derive(Debug)]
-pub struct DictResult {
-    pub query: String,
-    pub return_phrase: Option<String>,
-    pub lang: String,
-    pub phonetic_symbol: Option<String>,
-    pub translation: Option<CustomTranslation>,
-    pub web_translation: Option<Vec<WebTranslation>>,
-    pub wiki: Option<Wiki>,
-}
-#[derive(Debug)]
-pub struct CustomTranslation {
-    pub kind: String,
-    pub content: Vec<String>,
-}
-#[derive(Debug)]
-pub struct WebTranslation {
-    pub key: String,
-    pub value: Vec<String>,
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct YouDaoDict<'a> {
+    return_phrase: Option<Cow<'a, str>>,
+
+    lang: Cow<'a, str>,
+
+    phonetic_symbol: Option<Cow<'a, str>>,
+
+    custom_translation: Option<Translation<'a>>,
+
+    yodao_web_dict: Option<WebDict<'a>>,
+
+    recommend: Option<Vec<Cow<'a, str>>>,
+
+    wand_result: Option<WandResult<'a>>,
 }
 
-#[derive(Debug)]
-pub struct Wiki {
-    pub entry: String,
-    pub summary: String,
-}
+impl<'a> YouDaoDict<'a> {
+    // 打印基础信息
+    fn print_basic_info(&self) {
+        if let Some(value) = &self.return_phrase {
+            print!("  {}", style(value).bold().green());
+        }
 
-static EMPTY_STR: &str = "";
-pub fn get_xml_node_list<'a>(word: &String, xmldata: &'a str) -> DictResult {
-    let mut dict_rs = DictResult {
-        query: word.clone(),
-        return_phrase: None,
-        lang: String::from("eng"),
-        phonetic_symbol: None,
-        translation: None,
-        web_translation: None,
-        wiki: None,
-    };
+        if let Some(value) = &self.phonetic_symbol {
+            print!("{}", style(format!("  [{}]", value)).bold().green().dim());
+        }
+        println!()
+    }
 
-    let empty_str = "".to_string();
-    let config = ParserConfig {
-        trim_whitespace: true,
-        ..ParserConfig::default()
-    };
-    let parser = EventReader::new_with_config(xmldata.as_bytes(), config);
-
-    let mut temp_data = XmlData {
-        cdata: empty_str.clone(),
-        character: empty_str.clone(),
-    };
-    let mut path_buf = PathBuf::new();
-
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                if name.local_name != "yodaodict" {
-                    path_buf.push(name.local_name);
-                }
-            }
-            Ok(XmlEvent::CData(data)) => {
-                temp_data.cdata = data;
-            }
-            Ok(XmlEvent::Characters(data)) => {
-                temp_data.character = data;
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                if path_buf.ends_with("return-phrase") {
-                    dict_rs.return_phrase = Some(temp_data.cdata + &temp_data.character);
-                } else if path_buf.ends_with("custom-translation/type") {
-                    let dict = dict_rs.translation.get_or_insert(CustomTranslation {
-                        kind: empty_str.clone(),
-                        content: vec![],
-                    });
-                    dict.kind = temp_data.character;
-                } else if path_buf.ends_with("custom-translation/translation/content") {
-                    if let Some(trans) = &mut dict_rs.translation {
-                        trans.content.push(temp_data.cdata);
-                    };
-                } else if path_buf.ends_with("phonetic-symbol") {
-                    dict_rs.phonetic_symbol = Some(temp_data.character)
-                } else if path_buf.ends_with("web-translation/key") {
-                    let list = dict_rs.web_translation.get_or_insert(Vec::new());
-                    list.push(WebTranslation {
-                        key: temp_data.cdata + &temp_data.character,
-                        value: vec![],
-                    });
-                } else if path_buf.ends_with("web-translation/trans/value") {
-                    if let Some(list) = dict_rs.web_translation.as_mut().unwrap().last_mut() {
-                        list.value.push(temp_data.cdata + &temp_data.character);
-                    };
-                } else if path_buf.ends_with("web-translation/trans/value/cl") {
-                    if temp_data.cdata.len() > 0 || temp_data.character.len() > 0 {
-                        // 如果是计算机目录类别的，需要合并进前一个的value里面
-                        if let Some(web_translation) =
-                            dict_rs.web_translation.as_mut().unwrap().last_mut()
-                        {
-                            let last_value = web_translation.value.last_mut().unwrap();
-                            last_value.push_str("(");
-                            last_value.push_str(&temp_data.cdata);
-                            last_value.push_str(&temp_data.character);
-                            last_value.push_str(")");
-                        };
-                    }
-                } else if path_buf.ends_with("wiki-entry/info/data/entry") {
-                    dict_rs.wiki = Some(Wiki {
-                        entry: temp_data.character,
-                        summary: empty_str.clone(),
-                    });
-                } else if path_buf.ends_with("wiki-entry/info/data/summary") {
-                    dict_rs.wiki.as_mut().unwrap().summary = temp_data.character;
-                }
-
-                // println!("{}", path_buf.display());
-                temp_data.cdata = empty_str.clone();
-                temp_data.character = empty_str.clone();
-                if !name.to_string().eq("yodaodict") {
-                    path_buf.pop();
-                }
-                // println!("{}-{}", indent(depth), name);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
-            }
-            _ => {}
+    // 打印翻译结果
+    fn print_tanslation_content(&self) {
+        if let Some(list) = &self.custom_translation {
+            print_section_title("翻译", true);
+            list.translation
+                .iter()
+                .flat_map(|v| &v.content)
+                .for_each(|value| {
+                    println!("  {}", value);
+                });
+            println!()
         }
     }
-    dict_rs
+
+    // 打印网络翻译结果
+    fn print_web_tanslation_content(&self) {
+        if let Some(webdict) = &self.yodao_web_dict {
+            print_section_title("网络翻译", false);
+            webdict.web_translation.iter().for_each(|translation| {
+                print!("\n{} \n  ", style(&translation.key.trim()).magenta());
+
+                let str1 = translation
+                    .trans
+                    .iter()
+                    .map(|trans| {
+                        format!(
+                            "{}{}",
+                            &trans.value.trim(),
+                            &trans.cls.as_ref().map_or_else(|| { "" }, |x| { &x.cl })
+                        )
+                    })
+                    .collect::<Vec<_>>()
+                    .join(";");
+
+                print!("{}", str1)
+            });
+            println!()
+        }
+    }
+
+    // 打印 Wiki 结果
+    fn print_wiki(&self) {
+        if let Some(wand) = &self.wand_result {
+            print_section_title("Wiki", true);
+
+            let entity = &wand.wand.entity;
+            if let Some(wiki) = &entity.wiki_entry {
+                println!("{}", style(format!("[ {} ]", wiki.info.data.entry)).green());
+                println!("  {}", wiki.info.data.summary);
+            }
+            if let Some(person) = &entity.person_data {
+                println!(
+                    "{}",
+                    style(format!("[ {} ]", person.name.as_ref().unwrap())).green()
+                );
+                println!("  {}", person.summary.as_ref().unwrap());
+            }
+            println!()
+        }
+    }
+    fn print_to_stdio(&self) {
+        self.print_basic_info();
+        self.print_tanslation_content();
+        self.print_web_tanslation_content();
+        self.print_wiki();
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Translation<'a> {
+    translation: Vec<CustomTranslationContent<'a>>,
+}
+#[derive(Deserialize, Debug)]
+struct CustomTranslationContent<'a> {
+    content: Vec<Cow<'a, str>>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct WebDict<'a> {
+    web_translation: Vec<WebTranslation<'a>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct WebTranslation<'a> {
+    key: Cow<'a, str>,
+
+    trans: Vec<Value<'a>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Value<'a> {
+    value: Cow<'a, str>,
+    cls: Option<CL<'a>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct CL<'a> {
+    cl: Cow<'a, str>,
+}
+
+#[derive(Deserialize, Debug)]
+struct WandResult<'a> {
+    wand: Wand<'a>,
+}
+#[derive(Deserialize, Debug)]
+struct Wand<'a> {
+    entity: Entity<'a>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct Entity<'a> {
+    wiki_entry: Option<WikiEntry<'a>>,
+    person_data: Option<PersonData<'a>>,
+    artist: Option<Artist<'a>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct WikiEntry<'a> {
+    info: WikiInfo<'a>,
+}
+
+#[derive(Deserialize, Debug)]
+struct WikiInfo<'a> {
+    data: WikiInfoData<'a>,
+}
+
+#[derive(Deserialize, Debug)]
+struct WikiInfoData<'a> {
+    entry: Cow<'a, str>,
+    summary: Cow<'a, str>,
+}
+
+#[derive(Deserialize, Debug)]
+struct PersonData<'a> {
+    name: Option<Cow<'a, str>>,
+    summary: Option<Cow<'a, str>>,
+    achievement: Option<Cow<'a, str>>,
+    nationality: Option<Cow<'a, str>>,
+    job: Option<Cow<'a, str>>,
+    repWorks: Option<Cow<'a, str>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Artist<'a> {
+    name: Option<Cow<'a, str>>,
+    brif_name: Option<Cow<'a, str>>,
+}
+trait Translator {
+    fn translate_to_stdio(&mut self) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+impl<'a> YouDaoTranslator<'a> {
+    // 构建查询的url
+    pub fn build_query_url(&self) -> String {
+        let word = self.args.word.as_ref().unwrap();
+        let mut parsed = Url::parse(TRANSLATE_URL).unwrap();
+        parsed.query_pairs_mut().append_pair("q", word);
+        let url = parsed.to_string();
+        if self.args.debug {
+            let url_to_debug = style(&url).attr(Attribute::Underlined);
+            println!("url is {url_to_debug}",);
+        }
+        url
+    }
+
+    pub fn get_web_result_from_url(
+        &self,
+        url: String,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let resp = reqwest::blocking::get(url)?.text()?;
+        if self.args.debug {
+            println!(" xml data:{} \n", &resp)
+        }
+        Ok(resp)
+    }
+}
+impl<'a> Translator for YouDaoTranslator<'a> {
+    fn translate_to_stdio(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let url = self.build_query_url();
+        let xml_str = self.get_web_result_from_url(url)?;
+
+        let object: YouDaoDict = from_str(&xml_str)?;
+        if self.args.debug {
+            println!("result is {:?}", object);
+        }
+
+        object.print_to_stdio();
+        Ok(())
+    }
+}
+
+pub fn search_word(args: Args) {
+    let mut youdao = YouDaoTranslator { args: &args };
+
+    youdao.translate_to_stdio().unwrap();
+}
+
+pub fn print_section_title(title: &str, use_new_line: bool) {
+    if use_new_line {
+        println!(
+            "========{txt:^width$}========",
+            txt = style(title).magenta().dim(),
+            width = 8
+        );
+    } else {
+        print!(
+            "========{txt:^width$}========",
+            txt = style(title).magenta().dim(),
+            width = 8
+        );
+    }
 }
